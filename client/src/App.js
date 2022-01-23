5ecab4ba194acead3a9b1e5f1afd04ae6816a8cc
chanhopakr <comeghgh@gmail.com>
2022-01-23 16:44:42 +0900

    add ProxyserverMiddleware

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  useEffect(() => {
+    fetch("api")
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => setUsername(data.username));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <header className="App-header">
+          {username ? `Hello ${username}` : "Hello World"}
+        </header>
+      }
     </div>
   );
 }
